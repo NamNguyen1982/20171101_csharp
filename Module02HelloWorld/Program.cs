b06875e5473914d1334400f0ea875c23ab6cdbd5
Michell Cronberg <devcronberg@gmail.com>
2017-11-01 13:52:31 +0100

    02

--- Module02HelloWorld/Program.cs
+++ Module02HelloWorld/Program.cs
@@ -1,15 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
+/// <summary>
+/// 
+/// </summary>
 namespace Module02HelloWorld
 {
+    /// <summary>
+    /// 
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Denne fbfkjdgfklh 
+        /// </summary>
+        /// <param name="args">fhdhæ lgj dæ hjdg</param>
         static void Main(string[] args)
         {
+            //int am = 12;
+            //int antalMåneder = 12;
+
+            //WriteLine("ælsdfk");
+
+            int AntalMåneder = 12;
+
+            Console.WriteLine("ldfksgjdlkgj");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.Write("jgjgjg");
+            Console.Write("123456");
+
+            string navn = Console.ReadLine();
+            Console.ReadKey();
+            
+            
+
+        }
+
+        /// <summary>
+        /// Denne metode ------
+        /// </summary>
+        /// <param name="t">dfkjdf</param>
+        static void Test(int t) {
+
         }
     }
 }
