1b83bf7fe3b72507a206dd8914b00fc6bf1a0764
Michell Cronberg <devcronberg@gmail.com>
2017-11-02 14:55:04 +0100

    04

--- Module04Ref/Program.cs
+++ Module04Ref/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Module04Ref
 {
@@ -36,7 +38,7 @@ namespace Module04Ref
             h1.antalBen = 8;
             // hvad er h2.antalben???
 
-            
+
             // ---------------------------
 
             int mdr1 = 1000;
@@ -67,10 +69,148 @@ namespace Module04Ref
 
 
             var gns3 = månedsløn.Average();
-            
+
             // LINQ
             var res = månedsløn.Where(i => i > 3000).OrderBy(i => i);
             var res2 = månedsløn.OrderBy(i => i).GroupBy(i => i);
+
+            // --------------------------------
+
+            //string sti = "c:\\temp\\test.txt";
+            string sti = @"c:\temp\test.txt";
+            //string indholdAfFil = System.IO.File.ReadAllText(sti, System.Text.Encoding.Default);
+
+            string n1 = "K";
+            string n2 = "K";
+            if (n1 == n2)
+            {
+                Console.WriteLine("=");
+            }
+            else
+            {
+                Console.WriteLine("!=");
+            }
+
+            n1 = n1 + "*";
+
+            Console.WriteLine("start");
+            System.Diagnostics.Stopwatch s = new System.Diagnostics.Stopwatch();
+            s.Start();
+
+            string tmp = "";
+            for (int i = 0; i < 100000; i++)
+            {
+                tmp += "*";
+            }
+            tmp = tmp + "æjdædglkr" + " " + " lsdfgds ";
+            s.Stop();
+            Console.WriteLine(s.ElapsedMilliseconds);
+
+
+            Console.WriteLine("start");
+            System.Text.StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                sb.Append("*");
+            }
+            Console.WriteLine("stop");
+
+            string navn = "Mikkel Cronberg";
+            Console.WriteLine(navn.Trim());
+            string nyString = navn.ToUpper();
+            Console.WriteLine(nyString);
+            Console.WriteLine(navn.ToUpper());
+            Console.WriteLine(navn.Substring(1, 2));
+            var ar = navn.Split(' ');
+
+
+            DayOfWeek wd = DayOfWeek.Saturday;
+            switch (wd)
+            {
+                case DayOfWeek.Sunday:
+                case DayOfWeek.Saturday:
+
+
+                    break;
+
+                case DayOfWeek.Monday:
+                    break;
+                case DayOfWeek.Tuesday:
+                    break;
+                case DayOfWeek.Wednesday:
+                    break;
+                case DayOfWeek.Thursday:
+                    break;
+                case DayOfWeek.Friday:
+                    break;
+                default:
+                    break;
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int i = 0; i < 10; i = i + 2)
+            {
+                Console.WriteLine(i);
+            }
+            for (int i = 10; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 2)
+                    continue;
+                Console.WriteLine(i);
+                if (i == 5)
+                    break;
+            }
+
+            Console.WriteLine("Færdig");
+
+
+            int[] testArray = { 1, 05, 51, 7, 1, 8, 3, 4, 1, 5 };
+            Console.WriteLine(testArray);
+
+            // Kommasep
+            var ks = string.Join(", ", testArray);
+            Console.WriteLine(ks);
+
+            // Vis array som text (xml)
+            XmlSerializer serializer = new XmlSerializer(testArray.GetType());
+            System.Text.StringBuilder sbw = new StringBuilder();
+            using (StringWriter writer = new StringWriter(sbw))
+            {
+                serializer.Serialize(writer, testArray);
+            }
+            Console.WriteLine(sbw.ToString());
+
+            
+            // vis array som text (json)
+            string json = Newtonsoft.Json.JsonConvert.SerializeObject(månedsløn, Newtonsoft.Json.Formatting.Indented);
+            Console.WriteLine(json);
+
+
+            Hund[] hunde = new Hund[2];
+
+            hunde[0] = new Hund();
+            hunde[0].navn = "a";
+            hunde[0].antalBen = 4;
+
+            hunde[1] = new Hund();
+            hunde[1].navn = "b";
+            hunde[1].antalBen = 3;
+
+            string json2 = Newtonsoft.Json.JsonConvert.SerializeObject(hunde, Newtonsoft.Json.Formatting.Indented);
+            Console.WriteLine(json2);
+
+
+
 
         }
     }
